1052aacebdea5a3a0b45116db697d320a5f55aec
YixueYin <9010182+yixueyin@user.noreply.gitee.com>
2021-05-16 17:35:22 +0800

    事务维护更新

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
         <h3>Welcome this is staff home page</h3>
+        <div><img src="../assets/bg1.gif" alt=""></div>
     </div>
 </template>
+
+<style scoped>
+img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+</style>
